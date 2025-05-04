f973bfc147d8621a81de132eb187945ea9b23594
SaiSridharParimi <parimi.sai@northeastern.edu>
2025-05-04 12:50:48 -0400

    Update Patient with Custom Exceptions

--- patient-service/src/main/java/com/patient/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/patient/patient_service/controller/PatientController.java
@@ -2,13 +2,19 @@ package com.patient.patient_service.controller;
 
 import com.patient.patient_service.dto.PatientRequestDTO;
 import com.patient.patient_service.dto.PatientResponseDTO;
+import com.patient.patient_service.dto.validators.CreatePatientValidatorGroup;
+import com.patient.patient_service.model.Patient;
 import com.patient.patient_service.service.PatientService;
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -24,9 +30,15 @@ public class PatientController {
     }
 
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> addPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> addPatient(@Validated({Default.class, CreatePatientValidatorGroup.class})
+                                                             @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
 }
--- patient-service/src/main/java/com/patient/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/patient/patient_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.patient.patient_service.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,12 +13,30 @@ import java.util.Map;
 @ControllerAdvice //Cross Cutting Concerns
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e){
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(
                 error->errors.put(error.getField(), error.getDefaultMessage())
         );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String,String>> emailExists(EmailAlreadyExistsException emailAlreadyExistsException){
+        Map<String, String> errors = new HashMap<>();
+        log.warn("Email already exists with this {}",  emailAlreadyExistsException.getMessage());
+        errors.put("message", "Email already exists");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> patientNotFound(PatientNotFoundException patientNotFoundException){
+        log.warn("Patient not found with ID {}", patientNotFoundException.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Patient not found with the specified ID");
         return ResponseEntity.badRequest().body(errors);
     }
 
