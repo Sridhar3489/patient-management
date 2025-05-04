b45bae0f038bb9854570503a2a7e04af54c052e9
SaiSridharParimi <parimi.sai@northeastern.edu>
2025-05-04 00:22:29 -0400

    Added exceptions

--- patient-service/src/main/java/com/patient/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/patient/patient_service/mapper/PatientMapper.java
@@ -1,9 +1,13 @@
 package com.patient.patient_service.mapper;
 
+import com.patient.patient_service.dto.PatientRequestDTO;
 import com.patient.patient_service.dto.PatientResponseDTO;
 import com.patient.patient_service.model.Patient;
 
+import java.time.LocalDate;
+
 public class PatientMapper {
+
     public static PatientResponseDTO toDTO(Patient patient){
         PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
         patientResponseDTO.setId(patient.getId().toString());
@@ -13,4 +17,15 @@ public class PatientMapper {
         patientResponseDTO.setEmail(patient.getEmail());
         return patientResponseDTO;
     }
+
+    public static Patient toPatient(PatientRequestDTO patientRequestDTO){
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+
 }
