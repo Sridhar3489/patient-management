433bbd972259b13668317976d6534d0dfeacbb2c
SaiSridharParimi <parimi.sai@northeastern.edu>
2025-05-06 12:22:12 -0400

    JWT validation

--- auth-service/src/main/java/com/patient/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/patient/auth_service/service/AuthService.java
@@ -3,6 +3,8 @@ package com.patient.auth_service.service;
 import com.patient.auth_service.dto.LoginRequestDTO;
 import com.patient.auth_service.model.User;
 import com.patient.auth_service.util.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import java.util.Optional;
 @Service
 public class AuthService {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
     @Autowired
     private UserService userService;
 
@@ -25,6 +28,16 @@ public class AuthService {
         return userService.findUserByEmail(loginRequestDTO.getEmail())
                 .filter(u-> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
                 .map(u-> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+    }
+
+    public boolean validateToken(String token){
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (Exception e){
+            log.error("Unable to validate token {}", e.getMessage());
+            return false;
+        }
     }
 
 }
