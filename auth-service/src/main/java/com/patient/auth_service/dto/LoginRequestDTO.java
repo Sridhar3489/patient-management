5d99a80a6073ad36234e67326b42f422cd162725
SaiSridharParimi <parimi.sai@northeastern.edu>
2025-05-06 00:28:03 -0400

    auth-service init

---
+++ auth-service/src/main/java/com/patient/auth_service/dto/LoginRequestDTO.java
@@ -0,0 +1,32 @@
+package com.patient.auth_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    public @NotBlank(message = "Email is required") @Email(message = "Email is invalid") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is required") @Email(message = "Email is invalid") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String password) {
+        this.password = password;
+    }
+}
